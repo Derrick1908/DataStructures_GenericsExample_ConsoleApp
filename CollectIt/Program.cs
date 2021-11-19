fbaf89e3ee5f48c8085170603147d22c5e91942d
Derrick Nazareth <derrick.nazareth@teknorix.com>
2021-11-19 14:11:11 +0530

    Implemented Interfaces to Define how Employees (Objects) are Compared and Sorted

--- CollectIt/Program.cs
+++ CollectIt/Program.cs
@@ -3,21 +3,65 @@ using System.Collections.Generic;
 
 namespace CollectIt
 {
+    public class EmployeeComparer : IEqualityComparer<Employee>, 
+                                    IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            return String.Compare(x.Name, y.Name);
+        }
+
+        public bool Equals(Employee x, Employee y)
+        {
+            return String.Equals(x.Name, y.Name);
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            return obj.Name.GetHashCode();      //Returns the HashCode of the Empployers Name (string) so that it can be used by the HashSet for the case when Two Employees with the Same Name will have the Same Hash Code and thus prevent Duplicates from being Inserted.
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //var employeesByName = new Dictionary<string, List<Employee>>();
-            //var employeesByName = new SortedDictionary<string, List<Employee>>();       //Same as Dictionary but only Sorts the Values based on their Key Values. Note that if Keys are of Value Types it knows how to Sort. But if Keys are of Reference Types, then it has to be specified how to sort.
-            var employeesByName = new SortedList<string, List<Employee>>();       //A Sorted List is more efficient when only Iteration of the List is Involved. Sorted Dicitonary should be used when Inserting or Deleting Entries or Indexing is involved as it is more efficient for those operations.
+            //var departments = new Dictionary<string, List<Employee>>();
+            //var departments = new SortedDictionary<string, List<Employee>>();       //Used a Sorted Dictionary to Sort the Departments
+            //var departments = new SortedDictionary<string, HashSet<Employee>>();       //Used a HashSet to Store only Unique Employees per Department
+            var departments = new SortedDictionary<string, SortedSet<Employee>>();       //Used a SortedSet to Store both Unique Employees per Department as well as Alphabetically Sorted. Note that a SortedSet is just a HashSet but sorts the Entries.
 
-            employeesByName.Add("Sales", new List<Employee> { new Employee(), new Employee(), new Employee() });
-            employeesByName.Add("Engineering", new List<Employee> { new Employee(), new Employee() });
+            #region HashSet
+            /*
+            departments.Add("Sales", new HashSet<Employee>(new EmployeeComparer()));        //One of the Overloaded Constructors of HashSet can receive a Class that Implements IEqualityComparer
+            departments["Sales"].Add(new Employee { Name = "Joy" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
 
-            foreach (var item in employeesByName)
+            departments.Add("Engineering", new HashSet<Employee>(new EmployeeComparer()));      //This means thta use the Object of EmployeeComparer to Compare the Two Objects of the Employee Class
+            departments["Engineering"].Add(new Employee { Name = "Scott" });
+            departments["Engineering"].Add(new Employee { Name = "Alex" });
+            departments["Engineering"].Add(new Employee { Name = "Dani" }); */
+            #endregion
+
+            #region SortedSet
+            departments.Add("Sales", new SortedSet<Employee>(new EmployeeComparer()));        //One of the Overloaded Constructors of SortedSet can receive a Class that Implements not IEqualityComparer but IComparer (becoz here it not only has to Compare both the Values whether they are equal or not but here it has to Sort them as well)
+            departments["Sales"].Add(new Employee { Name = "Joy" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
+
+            departments.Add("Engineering", new SortedSet<Employee>(new EmployeeComparer()));      //This means thta use the Object of EmployeeComparer to Compare the Two Objects of the Employee Class
+            departments["Engineering"].Add(new Employee { Name = "Scott" });
+            departments["Engineering"].Add(new Employee { Name = "Alex" });
+            departments["Engineering"].Add(new Employee { Name = "Dani" });
+            #endregion
+
+            foreach (var department in departments)
             {
-                Console.WriteLine("The Count of Employees for {0} is {1}",
-                    item.Key, item.Value.Count);
+                Console.WriteLine(department.Key);
+                foreach (var employee in department.Value)
+                {
+                    Console.WriteLine("\t" + employee.Name);
+                }
             }
             Console.ReadKey();
         }
