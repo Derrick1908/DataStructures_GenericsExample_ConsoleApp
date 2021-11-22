0a1b4e66e879abb8c4bfe10d10f0ac4c5512a640
Derrick Nazareth <derrick.nazareth@teknorix.com>
2021-11-22 15:13:44 +0530

    Added a New Project to the Solution to demonstarte further topics of Generics; Added a Model Class and a Data Access Class with Generic Constraints; Added Entity Framework to the Project;

---
+++ QueryIt/DataAccess.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryIt
+{
+    public class EmployeeDb : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public interface IRepository<T> : IDisposable       //Interface is Generic becoz we could have respositories to Accounts, Employees, etc
+    {
+        void Add(T newEntity);
+        void Delete(T entity);
+        T FindById(int id);
+        IQueryable<T> FindAll();
+        int commit();
+    }
+
+    public class SqlRepository<T> : IRepository<T> where T : class //This is a Generic Constraint indicating that the Type of T must be of class type i.e. Reference Type and not Value Type
+    {
+        DbContext _ctx;
+        DbSet _set;
+        public SqlRepository(DbContext ctx)
+        {
+            _ctx = ctx;
+            _set = _ctx.Set<T>();
+        }
+        public void Add(T newEntity)
+        {
+            _set.Add(newEntity);
+        }
+
+        public int commit()
+        {
+            return _ctx.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
